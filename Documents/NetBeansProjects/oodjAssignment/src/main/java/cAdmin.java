a35f8fbf1a7988f2b3c4c00a012c9cec1a975381
calvinlim <calvinlim@192.168.0.112>
2023-02-28 00:56:58 +0800

    to Admin pages

--- Documents/NetBeansProjects/oodjAssignment/src/main/java/cAdmin.java
+++ Documents/NetBeansProjects/oodjAssignment/src/main/java/cAdmin.java
@@ -4,6 +4,7 @@
  */
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashSet;
@@ -36,7 +37,7 @@ public class cAdmin {
         return userInfoList;
     }
     
-        public String[] getDistinctRoles() {
+    public String[] getDistinctRoles() {
         Set<String> roles = new HashSet<>();
         try (BufferedReader br = new BufferedReader(new FileReader("userinfo.txt"))) {
             String line;
@@ -49,5 +50,28 @@ public class cAdmin {
         }
         return roles.toArray(new String[0]);
     }
+    
+    public boolean saveUserInfo(String role, String username, String password) {
+        // Check if the username already exists in the file
+        for (String[] userInfo : userInfoList) {
+            if (userInfo[1].equals(username)) {
+                // Username already exists, return false to indicate failure to save
+                return false;
+            }
+        }
+        
+        // Open the userinfo.txt file for writing
+        try (FileWriter writer = new FileWriter("userinfo.txt", true)) {
+            // Write the user information to the file
+            writer.write(role + "," + username + "," + password + "\n");
+            // Add the new user info to userInfoList
+            userInfoList.add(new String[] {role, username, password});
+            return true;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
 }
 
